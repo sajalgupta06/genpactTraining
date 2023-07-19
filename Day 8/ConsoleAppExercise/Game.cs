8ff3cf35a59a64f18385fad3165cce196602d925
Sajal Gupta <guptasajal6@gmail.com>
2023-07-19 11:35:16 +0530

    day 8 erercise

---
+++ Day 8/ConsoleAppExercise/Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExercise
+{
+    
+    public class Game
+    {
+        public int energyLevel = 1;
+        public int winningProb = 100;
+
+
+        public void zeroSpin() { this.energyLevel =1; this.winningProb = 100; }
+
+        public void firstSpin()
+
+        {
+            this.energyLevel +=1;
+            this.winningProb += 10;
+        }
+
+        public void secondSpin() { this.energyLevel += 2; this.winningProb = +20; }
+        public void thirdSpin () { this.energyLevel-= 3; this.winningProb -= 30; }
+
+        public void fourthSpin () { this.energyLevel += 4; this.winningProb += 40; }
+
+        public void fifthSpin() { this.energyLevel -= 5; this.winningProb -= 50; }
+
+
+        public void sixthSpin() { this.energyLevel -=1; this.winningProb -= 60; }
+
+
+        public void SevenSpin() { this.energyLevel += 1; this.winningProb -= 70; }
+
+
+        public void EightSpin() { this.energyLevel -= 2; this.winningProb -= 20; }
+
+        public void NinthSpin() { this.energyLevel -= 3; this.winningProb -= 30; }
+
+        public void TenthSpin() { this.energyLevel += 10; this.winningProb += 100; }
+
+    }
+}
---
+++ Day 8/ConsoleAppExercise/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExercise
+{
+
+    public delegate void GameDel();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name:");
+            string name = Console.ReadLine();
+
+            
+            int spin = 1;
+            Game game = new Game();
+            GameDel gameDel = new GameDel(game.zeroSpin);
+            while (spin <= 5)
+            {
+                Console.WriteLine("Enter Your Lucky Number Between 1 to 10");
+                int num = int.Parse(Console.ReadLine());
+              
+                switch (num)
+                {
+                    case 1:
+                        {
+                            gameDel += new GameDel(game.firstSpin);
+                            break;
+
+                        }
+                    case 2:
+                        {
+                            gameDel += new GameDel(game.secondSpin);
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            gameDel += new GameDel(game.thirdSpin);
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            gameDel += new GameDel(game.fourthSpin);
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            gameDel += new GameDel(game.fifthSpin);
+                            break;
+                        }
+
+
+                    case 6:
+                        {
+                            gameDel += new GameDel(game.sixthSpin);
+                            break;
+                        }
+
+                    case 7:
+                        {
+                            gameDel += new GameDel(game.SevenSpin);
+                            break;
+                        }
+
+                    case 8:
+                        {
+                            gameDel += new GameDel(game.EightSpin);
+                            break;
+                        }
+
+                    case 9:
+                        {
+                            gameDel += new GameDel(game.NinthSpin);
+                            break;
+                        }
+                    case 10:
+                        {
+                            gameDel += new GameDel(game.TenthSpin);
+                            break;
+                        }
+
+                }
+
+                spin++;
+            }
+
+          
+            if(game.energyLevel>=4 && game.winningProb > 60)
+            {
+                Console.WriteLine("Winner");
+            }
+            else if (game.energyLevel>=1 && game.winningProb>50)
+            {
+                Console.WriteLine("Runner up");
+            }
+            else
+            {
+                Console.WriteLine("Looser");
+            }
+         
+          Console.ReadKey();
+
+
+        }
+    }
+}
